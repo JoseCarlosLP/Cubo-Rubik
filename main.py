47273049eae719fa4faea62f05083a256619f4e5
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-04-11 22:46:55 -0400

    Se implemento la funcionalidad para girar la cara trasera del cubo en sentido horario y antihorario

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ class CuboRubik:
     def girar_cara_frontal(self, sentido='antihorario'):
         orden = ['U', 'R', 'D', 'L']
         a = 0
-        b = 2
+        b = -1
         if sentido == 'horario':
             orden = ['U', 'L', 'D', 'R']
-            a = 2
+            a = -1
             b = 0
         temp_cara_u = self.caras[orden[0]].piezas[-1]
         self.caras[orden[0]].piezas[-1] = [fila[a] for fila in self.caras[orden[1]].piezas]
@@ -34,6 +34,22 @@ class CuboRubik:
             self.caras[orden[1]].piezas[i][a] = self.caras[orden[2]].piezas[0][i]
             self.caras[orden[3]].piezas[i][b] = temp_cara_u[i]
         self.caras[orden[2]].piezas[0] = temp_cara_lado
+
+    def girar_cara_trasera(self, sentido='antihorario'):
+        orden = ['U', 'L', 'D', 'R']
+        a = 0
+        b = -1
+        if sentido == 'horario':
+            orden = ['U', 'R', 'D', 'L']
+            a = -1
+            b = 0
+        temp_cara_u = self.caras[orden[0]].piezas[0]
+        self.caras[orden[0]].piezas[0] = [fila[a] for fila in self.caras[orden[1]].piezas]
+        temp_cara_lado = [fila[b] for fila in self.caras[orden[3]].piezas]
+        for i in range(len(self.caras[orden[1]].piezas)):
+            self.caras[orden[1]].piezas[i][a] = self.caras[orden[2]].piezas[-1][i]
+            self.caras[orden[3]].piezas[i][b] = temp_cara_u[i]
+        self.caras[orden[2]].piezas[-1] = temp_cara_lado
 
     def girar_cara_derecha(self, sentido='antihorario'):
         orden = ['D', 'F', 'U', 'B']
@@ -45,6 +61,17 @@ class CuboRubik:
             self.caras[orden[1]].piezas[i][-1] = self.caras[orden[2]].piezas[i][-1]
             self.caras[orden[2]].piezas[i][-1] = self.caras[orden[3]].piezas[i][0]
             self.caras[orden[3]].piezas[i][0] = temp_cara[i]
+
+    def girar_cara_izquierda(self, sentido='antihorario'):
+        orden = ['U', 'F', 'D', 'B']
+        if sentido == 'horario':
+            orden = ['D', 'F', 'U', 'B']
+        temp_cara = [fila[0] for fila in self.caras[orden[0]].piezas]
+        for i in range(len(temp_cara)):
+            self.caras[orden[0]].piezas[i][0] = self.caras[orden[1]].piezas[i][0]
+            self.caras[orden[1]].piezas[i][0] = self.caras[orden[2]].piezas[i][0]
+            self.caras[orden[2]].piezas[i][0] = self.caras[orden[3]].piezas[i][-1]
+            self.caras[orden[3]].piezas[i][-1] = temp_cara[i]
 
     def mostrar_estado(self):
         for cara in self.caras:
@@ -67,9 +94,25 @@ if __name__ == '__main__':
     # print("MOV = F'")
     # cubo.mostrar_estado()
 
-    print("MOV = R'")
-    cubo.girar_cara_derecha()
-    cubo.mostrar_estado()
-    print('MOV = R')
-    cubo.girar_cara_derecha('horario')
-    cubo.mostrar_estado()
+    # print("MOV = R'")
+    # cubo.girar_cara_derecha()
+    # cubo.mostrar_estado()
+    # print('MOV = R')
+    # cubo.girar_cara_derecha('horario')
+    # cubo.mostrar_estado()
+
+    # print('MOV = L')
+    # cubo.girar_cara_izquierda('horario')
+    # cubo.mostrar_estado()
+    # print("MOV = L'")
+    # cubo.girar_cara_izquierda()
+    # cubo.mostrar_estado
+
+    # print("MOV = B'")
+    # cubo.girar_cara_trasera()
+    # cubo.mostrar_estado()
+    # print('MOV = B')
+    # cubo.girar_cara_trasera('horario')
+    # cubo.mostrar_estado()
+
+
